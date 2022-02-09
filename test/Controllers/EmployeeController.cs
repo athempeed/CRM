8f792487381e9c35520b77ca27649fd1de592461
Deepak Singh <Deepak_S_Mehta@dell.com>
2022-02-09 20:00:31 +0800

    initial commit

---
+++ test/Controllers/EmployeeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace test.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("index");
+        }
+    }
+}
